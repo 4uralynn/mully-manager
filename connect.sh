d083735963dd6e38af0034f11124142bcbdccc54
Laura Engram <lauracornwell@ieee.org>
2022-02-01 00:46:22 -0800

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,9 +1,6 @@
 status=$( tail -n1 log.txt | grep down | cut -d" " -f6 )	
 dateandtime=$( date +"%Y-%m-%d %T" )
 if [ $status ]; then 
-	sudo ip rule add from 10.0.0.113 table 128
-	sudo ip route add table 128 to 10.0.0.113/32 dev eth0
-	sudo ip route add table 128 default via 10.0.0.1
 	currprocess=$( ps all | tail -n1 | tr -s " " | cut -d" " -f3 )
 	mullprocess=$(($currprocess+1))
 	echo "$dateandtime ($USER) NEW connection[$mullprocess]: up" >> log.txt
